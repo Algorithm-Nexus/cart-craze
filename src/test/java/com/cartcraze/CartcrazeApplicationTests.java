6a25530c146f25199c83e12227111ea98ca462b1
Aman-62 <aman230630@gmail.com>
2024-10-14 15:01:45 +0530

    Initial Commit

---
+++ src/test/java/com/cartcraze/CartcrazeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cartcraze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartcrazeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
